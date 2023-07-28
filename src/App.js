21fbd0aa1206c75e3dfc6540d17268d6c7f50d0a
Nitin Singh Negi <nitinnegi71741@gmail.com>
2023-07-28 17:43:48 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Display from './component/Display';
+
 
 import Template from './component/Template';
 
@@ -11,11 +11,10 @@ function App() {
       <BrowserRouter>
       <Routes>
       <Route path='/' element={<Template/>}/>
-      {/* <Route path='/Display' element={<Display/>}/> */}
+      
       </Routes>
       </BrowserRouter>
-      {/* <Template/>
-    <Display/> */}
+  
     </div>
   );
 }
